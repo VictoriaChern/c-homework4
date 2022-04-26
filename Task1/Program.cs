505ddc7e51dcb1493a1cbc4cffef6677c80a6b06
Viktoria.Cherniavskaya <victoria.kli1707@gmail.com>
2022-04-26 23:53:26 +0300

    add homework4

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+int A = AskNumber("A");
+int B = AskNumber("B");
+WriteLine($"{A},{B} => {Exponentiation(A,B)}");
+//WriteLine($"{A},{B} => {Math.Pow(A,B)}");
+
+int AskNumber(string name) {
+    Write($"Введите число {name}: ");
+    return int.Parse(ReadLine());
+}
+
+int Exponentiation(int A, int B) {
+    int result = 1;
+    for(int i = 0; i < B; i++) {
+        result = result*A;
+    }
+    return result;
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+string inputNumber = ReadLine();
+int[] numbers = ConvertToIntArray(inputNumber);
+WriteLine($"{inputNumber} -> {Sum(numbers)}");
+
+int[] ConvertToIntArray(string str) {
+    int[] numbers = new int[str.Length];
+    for(int i = 0; i < str.Length; i++) {
+        numbers[i] = (int) Char.GetNumericValue(str[i]);
+    }
+    return numbers;
+}
+
+int Sum(int[] numbers) {
+    int sum = 0;
+    for(int i = 0; i < numbers.Length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+
+
